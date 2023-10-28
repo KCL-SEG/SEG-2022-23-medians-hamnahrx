867df9035b6439c5653dbf5ffe9275a779c5874a
Hamnah Rassen <hamnah.1.rassen@kcl.ac.uk>
2023-10-28 20:39:43 +0100

    Implemented code to calculate and return median of list.

--- median.py
+++ median.py
@@ -3,10 +3,19 @@
 
 while True:
     try:
-        print("Enter a list of numbers separated by commas: ")
-        numbers = [float(value) for value in input().split(",")]
+        input_string = input("Enter a list of numbers separated by commas: ")
+        numbers = [float(value) for value in input_string.split(",")]
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+numbers.sort()
+n = len(numbers)
+
+if n % 2 == 0:
+    median = (numbers[n // 2] + numbers[(n // 2) - 1]) / 2
+else:
+    median = numbers[n // 2]
+
+print("The median of this list is: ", median)
